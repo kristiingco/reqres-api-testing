0632bef8339c8c322656d8b7d38cae95be363965
Kristiana Ingco <kristiana.d.ingco@gmail.com>
2024-03-07 17:08:54 -0800

    Add test for validating headers

--- cypress/e2e/api.cy.js
+++ cypress/e2e/api.cy.js
@@ -1,5 +1,17 @@
-describe("template spec", () => {
-    it("passes", () => {
-        //TODO
+/// <reference types="Cypress"/>
+
+describe("API Tests", () => {
+    it("should validate headers", () => {
+        cy.request("/users/2").as("user");
+
+        cy.get("@user")
+            .its("headers")
+            .its("content-type")
+            .should("include", "application/json");
+
+        cy.get("@user")
+            .its("headers")
+            .its("connection")
+            .should("include", "keep-alive");
     });
 });
